49b67bab9e93ab72234c0b40d6e0b33af3f7d339
Gabrielle Rodrigues <silva111gabi16@gmail.com>
2024-09-05 04:15:30 -0300

    botao menu mobile

--- js/menu.js
+++ js/menu.js
@@ -8,3 +8,14 @@ document.addEventListener('scroll', ()=>{
         navBar.classList.remove('rolar')
     }
 })
+
+// ----------------------------------------------------------
+
+let btnMenuMob = document.querySelector('#btn-menu-mob')
+let line1 = document.querySelector('.line-menumob-1')
+let line2 = document.querySelector('.line-menumob-2')
+
+document.addEventListener('click', ()=>{
+    line1.classList.toggle('ativo1')
+    line2.classList.toggle('ativo2')
+})
